abec1b7c33151877c858c0ee1eebc06abdbd0890
Semo04001 <112382699+Semo04001@users.noreply.github.com>
2022-10-17 08:44:34 +0200

    Träna

---
+++ Program copy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace CODING
+{
+        class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr= new int[5];
+            for(int i=0; i<5; i++)
+            {
+                Console.WriteLine("mata in tal "+(i+1));
+                arr[i]=int.Parse(Console.ReadLine());
+            }
+            foreach(int e in arr)
+            {
+                Console.Write(arr[e]);
+            }
+        }
+    }
+}
